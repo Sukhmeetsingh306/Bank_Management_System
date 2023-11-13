bd0eb14e48b1156f4098746e7a49eb35ea81bbab
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2023-11-13 13:10:47 +0530

    Conn.java

--- Bank Management System/src/bank/management/system/Conn.java
+++ Bank Management System/src/bank/management/system/Conn.java
@@ -10,7 +10,7 @@ public class Conn {
     public Conn() {
         try{
              Class.forName("com.mysql.cj.jdbc.Driver");
-             c = DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem", "root", "sukhmeet306singh");
+             c = DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem", "root", "/*Enter the SOL Password*/");
              s = c.createStatement();
              
         } catch (Exception e) {
